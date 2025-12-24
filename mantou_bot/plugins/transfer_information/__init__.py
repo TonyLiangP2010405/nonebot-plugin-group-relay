4aac5104a735e3df92d1b2f390e38730ae172647
liangpuyue <m13614526758@163.com>
2025-12-24 09:50:22 +0800

    add open, close plugin function

--- mantou_bot/plugins/transfer_information/__init__.py
+++ mantou_bot/plugins/transfer_information/__init__.py
@@ -4,34 +4,44 @@ from nonebot.log import logger
 import json
 from pathlib import Path
 
+_plugin_information = {}
 _plugin_state = None
 STATE_FILE = Path(__file__).parent / "plugin_information.json"
 open_switch = on_command("开启群聊监听", priority=10, block=True)
+close_switch = on_command("关闭群聊监听", priority=10, block=True)
 listener_group = on_command("", priority=10, block=True)
 
-def _load_state():
-    global _plugin_state
+def _load_information():
+    global _plugin_information
     if STATE_FILE.exists():
         try:
-            _plugin_state = json.loads(STATE_FILE.read_text("utf-8"))
+            _plugin_information = json.loads(STATE_FILE.read_text("utf-8"))
+            _plugin_state = _plugin_information["plugin_state"]
         except Exception:
-            _plugin_state = None
+            _plugin_information = {"plugin_state": None}
+            STATE_FILE.write_text(
+                json.dumps({"plugin_state": ""}, ensure_ascii=False, indent=2),
+                "utf-8",
+            )
     else:
-        _plugin_state = None
+        STATE_FILE.write_text(
+            json.dumps({"plugin_state":""}, ensure_ascii=False, indent=2),
+            "utf-8",
+        )
 
 
-def _save_state():
+def _save_information():
     STATE_FILE.write_text(
-        json.dumps(_plugin_state, ensure_ascii=False, indent=2),
+        json.dumps(_plugin_information, ensure_ascii=False, indent=2),
         "utf-8",
     )
 
 @open_switch.handle()
 async def open_plugin(bot: Bot, event: Event):
-    global _plugin_state
-    _load_state()
-    _plugin_state = True
-    _save_state()
+    global _plugin_information
+    _load_information()
+    _plugin_information["plugin_state"] = True
+    _save_information()
     content = "插件已开启"
     await bot.send_group_msg(
         group_id=event.group_id,
@@ -39,6 +49,18 @@ async def open_plugin(bot: Bot, event: Event):
     )
     await open_switch.finish()
 
+@close_switch.handle()
+async def close_plugin(bot: Bot, event: Event):
+    global _plugin_information
+    _load_information()
+    _plugin_information["plugin_state"] = False
+    _save_information()
+    content = "插件已关闭"
+    await bot.send_group_msg(
+        group_id=event.group_id,
+        message=Message(content),
+    )
+    await close_switch.finish()
 
 @listener_group.handle()
 async def listen_group_message_test(bot: Bot, event: Event):
